4032a2d866d481191c4db955e1f6059428f818b3
hyeonsu-choi <chs.henesys@gmail.com>
2022-02-13 21:43:48 +0900

    ccp데이터관리 헤드/서브 데이터 분리(누락파일추가)

---
+++ src/viewmodel/CCPDataDetailViewModel.java
@@ -0,0 +1,48 @@
+package viewmodel;
+
+public class CCPDataDetailViewModel {
+	private String sensorName;
+	private String createTime;
+	private String event;
+	private String sensorValue;
+	private String judge;
+	private String improvementAction;
+	
+	public String getSensorName() {
+		return sensorName;
+	}
+	public void setSensorName(String sensorName) {
+		this.sensorName = sensorName;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getEvent() {
+		return event;
+	}
+	public void setEvent(String event) {
+		this.event = event;
+	}
+	public String getSensorValue() {
+		return sensorValue;
+	}
+	public void setSensorValue(String sensorValue) {
+		this.sensorValue = sensorValue;
+	}
+	public String getJudge() {
+		return judge;
+	}
+	public void setJudge(String judge) {
+		this.judge = judge;
+	}
+	public String getImprovementAction() {
+		return improvementAction;
+	}
+	public void setImprovementAction(String improvementAction) {
+		this.improvementAction = improvementAction;
+	}
+	
+}
