6644b9a50e1f32becedd6048be62c72ccee5e34d
hyeonsu-choi <chs.henesys@gmail.com>
2022-02-21 15:54:02 +0900

    db connection close 코드 추가

--- src/service/BiznoSubdomainPairService.java
+++ src/service/BiznoSubdomainPairService.java
@@ -2,21 +2,33 @@ package service;
 
 import java.sql.Connection;
 
+import org.apache.log4j.Logger;
+
 import dao.BiznoSubdomainPairDao;
 import mes.frame.database.JDBCConnectionPool;
 import model.BiznoSubdomainPair;
 
 public class BiznoSubdomainPairService {
 	private BiznoSubdomainPairDao bspDao;
+	private Connection conn;
+	
+	static final Logger logger = Logger.getLogger(BiznoSubdomainPairService.class.getName());
 	
 	public BiznoSubdomainPairService(BiznoSubdomainPairDao bspDao) {
 		this.bspDao = bspDao;
 	}
 	
 	public String getBizno(String subdomain) {
-		Connection conn = JDBCConnectionPool.getMasterDB();
+		BiznoSubdomainPair bsPair = null;
 		
-		BiznoSubdomainPair bsPair = bspDao.getBiznoSubdomainPair(conn, subdomain);
+		try {
+			conn = JDBCConnectionPool.getMasterDB();
+			bsPair = bspDao.getBiznoSubdomainPair(conn, subdomain);
+		} catch(Exception e) {
+			logger.error(e.getMessage());
+		} finally {
+		    try { conn.close(); } catch (Exception e) { /* Ignored */ }
+		}
 		
 		return bsPair.getBizNo();
 	}
